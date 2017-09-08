ed055058c7956ebb703464c742837a9ace797129
Niels de Vos <ndevos@redhat.com>
2017-09-08 16:40:56 +0200

    basic-gluster-duffy: retry in case testing hits the rate limit
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- common-scripts/basic-gluster-duffy.py
+++ common-scripts/basic-gluster-duffy.py
@@ -11,7 +11,7 @@
 # Please note, this is a basic script, there is no error handling and there are
 # no real tests for any exceptions. Patches welcome!
 
-import json, urllib, subprocess, sys, os
+import json, urllib, subprocess, sys, os, time
 
 url_base="http://admin.ci.centos.org:8080"
 ver=os.getenv("CENTOS_VERSION")
@@ -19,6 +19,10 @@ arch=os.getenv("CENTOS_ARCH")
 count=2
 server_script=os.getenv("SERVER_TEST_SCRIPT")
 client_script=os.getenv("CLIENT_TEST_SCRIPT")
+# delay for 5 minutes (duffy timeout for rate limiting)
+retry_delay=300
+# retry maximum 3 hours, that is 3 x 60 x 60 seconds 
+max_retries=((3 * 60 * 60) / retry_delay)
 
 # read the API key for Duffy from the ~/duffy.key file
 fo=open("/home/nfs-ganesha/duffy.key")
@@ -28,9 +32,23 @@ fo.close()
 # build the URL to request the system(s)
 get_nodes_url="%s/Node/get?key=%s&ver=%s&arch=%s&count=%s" % (url_base,api,ver,arch,count)
 
-# request the system
-dat=urllib.urlopen(get_nodes_url).read()
-b=json.loads(dat)
+# request the system(s)
+retries=0
+while retries < max_retries:
+    try:
+        dat=urllib.urlopen(get_nodes_url).read()
+        b=json.loads(dat)
+        # all is fine, break out of the loop
+        break
+    except ValueError, ve:
+        print("Failed to parse Duffy response: %s" % (dat))
+    except Error, e:
+        print("An unexpected error occured: %s" % (e))
+
+    retries+=1
+    print("Waiting %d seconds before retrying #%d..." % (retry_delay, retries))
+    time.sleep(retry_delay)
+
 
 # NFS-Ganesha Server (parameters need double escape, passed on ssh commandline)
 server_env="export GERRIT_HOST='%s'" % os.getenv("GERRIT_HOST")
